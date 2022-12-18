7d5bd3857528ffbcae521b89f7900fd3a41ecb9c
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-18 13:19:13 +0300

    Configuring cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  #Adding this module here means all of our controllers will be able to work with cookies
+  
 end
